24c1d71591b527a0374a1ea6575de005ad3917a8
Sadeem Ahmad <sadahmad@deloitte.com>
2024-05-14 16:05:29 +0300

    adding error handling to client

--- src/network/graphqlClient.ts
+++ src/network/graphqlClient.ts
@@ -1,6 +1,34 @@
-import {ApolloClient, InMemoryCache} from '@apollo/client';
+import {
+  ApolloClient,
+  ApolloLink,
+  HttpLink,
+  InMemoryCache,
+} from '@apollo/client';
+import {onError} from '@apollo/client/link/error';
+
+const httpLink = new HttpLink({
+  uri: 'https://graphql.anilist.co',
+  credentials: 'same-origin',
+});
+
+// error handling and formatting
+const errorLink = onError(({graphQLErrors, networkError}) => {
+  if (graphQLErrors) {
+    graphQLErrors.forEach(({message, locations, path}) =>
+      console.log(
+        `[GraphQL error]: Message: ${message}, Location: ${locations}, Path: ${path}`,
+      ),
+    );
+  }
+
+  if (networkError) {
+    console.log(`[Network error]: ${networkError}`);
+  }
+});
+
+// cached storage for the user token
 
 export const client = new ApolloClient({
-  uri: 'https://graphql.anilist.co',
+  link: ApolloLink.from([errorLink, httpLink]),
   cache: new InMemoryCache(),
 });
